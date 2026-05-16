1f8836000a89d1421a749b7674aafe5f1aafa7f4
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-05-16 11:56:07 +0530

    animation work

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placeholder.com',
+      },
+    ], // Add any image domains you'll use
+  },
+}
+
+module.exports = nextConfig
